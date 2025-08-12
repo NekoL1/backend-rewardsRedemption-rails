5927851e4e6ca7a6eb3862f3013ed55681dc61b7
XiuhuiL66 <xiuhui.liang@weclouddata.com>
2025-08-11 21:50:13 -0400

    delete unsubscribed

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,15 +1,13 @@
 class ProductChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    def subscribed
-      stream_from "product_#{params[:product_id]}"
-      Rails.logger.info("✅ Subscribing to stream: #{stream_name}")
-      stream_from stream_name
-    end
+    stream_from "product_#{params[:product_id]}"
+    Rails.logger.info("Subscribing to stream: #{stream_name}")
+    # stream_from stream_name
   end
 
-  def unsubscribed
-     # Any cleanup needed when channel is unsubscribed
-     Rails.logger.info("❌ Unsubscribed from product_#{params[:product_id]}")
-  end
+  # def unsubscribed
+  #    # Any cleanup needed when channel is unsubscribed
+  #    Rails.logger.info("Unsubscribed from product_#{params[:product_id]}")
+  # end
 end
